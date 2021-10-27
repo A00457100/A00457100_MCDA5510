44d2872bc0c241c5fb2f83df6a1ecbdc36692079
A00457100 <tulluri.murali1@gmail.com>
2021-10-27 20:20:41 -0300

    Assignment 1 - Traverse the directories
    Assignment 1 codes, output and logs have been added

---
+++ Assignment1/A00457100_Assignment1_code/A00457100_Assignment1/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A00457100_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+            string file = "/Users/muralitulluri/Documents/Output.txt";
+            string Skipped = "/Users/muralitulluri/Documents/Skipped_rows.txt";
+            string Log = "/Users/muralitulluri/Documents/Log_file.txt";
+            string header = "First Name,Last Name,Street Number,Street,City,Province,Postal Code,Country,Phone Number,email Address,Date";
+            string filePath1 = @"/Users/muralitulluri/Documents/Output.txt";
+            string skipped_rows = @"/Users/muralitulluri/Documents/Skipped_rows.txt";
+            using (StreamWriter outputFile = new StreamWriter(skipped_rows, true))
+            {
+                outputFile.WriteLine(header);
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(filePath1, true))
+            {
+                outputFile.WriteLine(header);
+            }
+            using (StreamWriter outputFile = new StreamWriter(Log, true))
+            {
+                outputFile.WriteLine("This program merges all the CSV files by rejecting any invalid records");
+                DirWalker traverse = new DirWalker();
+                traverse.walk(@"/Users/muralitulluri/Documents/Sample Data");
+                watch.Stop();
+                outputFile.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+                outputFile.WriteLine("Number of total correct lines excluding header are : " + File.ReadLines(file).Count());
+                outputFile.WriteLine("Number of total skipped lines are : " + File.ReadLines(Skipped).Count());
+
+            }
+        }
+    }
+
+
+    public class DirWalker
+    {
+
+        public void walk(String path)
+        {
+
+            string[] list = Directory.GetDirectories(path);
+            SimpleCSVParser simpleCSV = new SimpleCSVParser();
+            //int directory_count = 0;
+            //int file_count = 0;
+
+            if (list == null) return;
+
+            foreach (string dirpath in list)
+            {
+                if (Directory.Exists(dirpath))
+                {
+                    walk(dirpath);
+                       //Console.WriteLine("Dir:" + dirpath);
+                }
+            }
+           // directory_count = directory_count + list.count();
+            string[] fileList = Directory.GetFiles(path);
+            
+
+            // file_count = file_count + fileList.count();
+            foreach (string filepath in fileList)
+            {
+               // n = n + 1;
+               // if (n%100 == 0)
+               // {
+               //     Console.WriteLine("Processing the " + n + " st/th file");
+               // }
+                if (filepath.Contains(".csv"))
+                {
+                    char char1 = '\\';
+                    string[] path_split = filepath.Split(char1);
+                    string year1 = path_split[1];
+                    string month1="";
+                    string day = "";
+                    string date_custom = "";
+                    if (path_split[2].Length == 1)
+                    {
+                        month1 = "0" + path_split[2];
+                        //Console.WriteLine("File:" + month+"another month"+month1);
+                        // Console.WriteLine("File:" + date_custom);
+                    }
+                    else month1 = path_split[2];
+                    if (path_split[3].Length == 1)
+                    {
+                        day = "0" + path_split[3];
+                        //Console.WriteLine("File:" + month+"another month"+month1);
+                        // Console.WriteLine("File:" + date_custom);
+                    }
+                    else day = path_split[3];
+                    date_custom = year1 + "-" + month1 + "-" + day;
+                    //Console.WriteLine("File:" + date_custom);
+                    // Console.WriteLine("Call the file parser");
+                    simpleCSV.parse(@filepath, date_custom);
+                }
+
+            }
+            //Console.WriteLine("All " + n + " files are processed and the output files are generated below are the stats");
+
+        }
+
+
+
+    }
+
+  
+    public class SimpleCSVParser
+    {
+
+        public void parse(String fileName,String Date_Custom)
+        {
+            try
+            {
+                using (TextFieldParser parser = new TextFieldParser(fileName))
+                {
+                    TextFieldParser parser1 = new TextFieldParser(fileName);
+                    string filePath1 = @"/Users/muralitulluri/Documents/Output.txt";
+                    string skipped_rows= @"/Users/muralitulluri/Documents/Skipped_rows.txt";
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    int i = 1;
+                    int j;
+                    int k;
+                    List<string> rows = new List<string>();
+                    List<string> rows_skipped = new List<string>();
+                    while (!parser.EndOfData)
+                    {
+
+                            //Process row
+                            string[] fields = parser.ReadFields();
+                            string line = parser1.ReadLine()+ ","+Date_Custom;
+                            
+                           
+                            j = 0;
+                            k = 0;
+
+                            if (fields.Count()!=10)
+                            {
+                            //Console.WriteLine("Wrong number of fields in row number " + i);
+                            rows_skipped.Add("There are only "+ fields.Count()+" fields in row number " + i+ " of the file "+ fileName+"so skipping the row and the row data is "+line);
+                            k = 1;
+                            }
+                        foreach (string field in fields)
+                            {
+                                j = j+1;
+                            if (fields.Contains("First Name"))
+                            {
+                                //Console.WriteLine("Wrong number of fields in row number " + i);
+                                k = 1;
+                                break;
+                            }
+                            if (field == "" && k == 0)
+                                {
+                                
+                                    rows_skipped.Add("Field " + j + " is blank in row number " + i + " of the file " + fileName + " so skipping the row and the row data is " + line);
+                                    k = 1;
+                                break;
+                                }
+                            }
+                            if (k == 0)
+                            {
+                            /*  using (StreamWriter outputFile = new StreamWriter(filePath1, true))
+                              {
+                                  outputFile.WriteLine(line);
+                              }*/
+
+                            rows.Add(line);
+                            }
+                            i = i+1;
+                        }
+                    using (StreamWriter outputFile = new StreamWriter(filePath1, true))
+                    {
+                        foreach(string data in rows)
+                            outputFile.WriteLine(data);
+                    }
+                    using (StreamWriter outputFile = new StreamWriter(skipped_rows, true))
+                    {
+                        foreach (string data1 in rows_skipped)
+                            outputFile.WriteLine(data1);
+                    }
+                }
+
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe.StackTrace);
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Assignment1/A00457100_Assignment1_code/A00457100_Assignment1/README.txt
@@ -0,0 +1,19 @@
+﻿Assignment #1
+-------------------------------------------------------------------
+
+The goal of the assignment is to simply combine 3 of these programs into a single
+program that will recursively read a series of data files in CSV format and enter 
+them into a single file.
+
+1. Read the files from all the directories.
+2. Validated the below scenarios :
+   a) Check if there are exactly 10 fields in every file, if not reject the row/line
+   b) Check if any of the fields are empty, if so then reject the row/line
+   c) Noticed that some email IDs are Null@domain, but that doesnt seem invalid since null is still a text and can be a valid email id
+3. After validation, write all the successfull rows/lines into one single file named as Output.txt
+4. Write all the invalid records into one single file along with the reason for being invalid and their exact row number to a file named as Skipped_rows.txt
+5. Added Date field to all the rows/lines as per their directory location
+6. Created a log file to contain the number of valid rows, number of invalid rows, and the total execution time to the file Log_file.txt
+
+IMPORTANT :
+To make the program work, make sure to replace "/Users/muralitulluri/Documents/" with appropriate path where the source data files reside.
